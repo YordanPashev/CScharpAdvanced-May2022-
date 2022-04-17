869514c10deb3d9827da8aec64af8d045d187652
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-04-17 11:30:00 +0300

    Adding Problems Desctriptopn and Solulition to StacksAndQueues from P01 to P08

---
+++ StacksAndQueues-Lab/P03.SimpleCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            string expression = Console.ReadLine();
+
+            int[] digits = expression
+                .Split(new[] { " + ", " - "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            foreach (int digit in digits)
+            {
+                stack.Push((int)digit);
+            }
+            int result = 0;
+
+            for (int i = expression.Length - 1; i >= 0; i--)
+            {
+                char action = expression[i];
+
+                if (action == '+')
+                {
+                    result += stack.Pop();
+                }
+
+                else if (action == '-')
+                {
+                    result -= stack.Pop();
+                }
+            }
+
+            result += stack.Pop();
+            Console.WriteLine(result);
+        }
+    }
+}
