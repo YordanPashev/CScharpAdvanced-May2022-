0d8a3a502901330f00e2d534db8c32e383a6768f
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-04-20 23:33:45 +0300

    Adding Problem Solution to P06.Jagged-ArrayModification

--- MultidimensionalArrays/MultidimensionalArrays-Lab/P05.SquareWithMaximumSum/Program.cs
+++ MultidimensionalArrays/MultidimensionalArrays-Lab/P05.SquareWithMaximumSum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace P05.SquareWithMaximumSum
 {
@@ -6,7 +7,63 @@ namespace P05.SquareWithMaximumSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] matrixSize = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rows = matrixSize[0];
+            int columns = matrixSize[1];
+
+            int[,] matrix = new int[rows, columns];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currRowValues = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < columns; col++)
+                {
+                    matrix[row, col] = currRowValues[col];
+                }
+            }
+
+            int maxValue = int.MinValue;
+            int[] biggestSumSquareElements = new int[4];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int currSquareSum = 0;
+
+                if (row + 1 >= rows)
+                {
+                    break;
+                }
+
+                for (int col = 0; col < columns; col++)
+                {
+                    if (col + 1 >= columns)
+                    {
+                        break;
+                    }
+
+                    currSquareSum = matrix[row, col] + matrix[row + 1, col] + matrix[row, col + 1] + matrix[row + 1, col + 1];
+
+                    if (currSquareSum > maxValue)
+                    {
+                        maxValue = currSquareSum;
+                        biggestSumSquareElements[0] = matrix[row, col];
+                        biggestSumSquareElements[1] = matrix[row, col + 1];
+                        biggestSumSquareElements[2] = matrix[row + 1, col];
+                        biggestSumSquareElements[3] = matrix[row + 1, col + 1];
+                    }
+                }
+            }
+
+            Console.WriteLine($"{biggestSumSquareElements[0]} {biggestSumSquareElements[1]}");
+            Console.WriteLine($"{biggestSumSquareElements[2]} {biggestSumSquareElements[3]}");
+            Console.WriteLine(maxValue);
         }
     }
 }
--- MultidimensionalArrays/MultidimensionalArrays-Lab/P06.Jagged-ArrayModification/Program.cs
+++ MultidimensionalArrays/MultidimensionalArrays-Lab/P06.Jagged-ArrayModification/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace P06.Jagged_ArrayModification
 {
@@ -6,7 +7,56 @@ namespace P06.Jagged_ArrayModification
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int matrixSize = int.Parse(Console.ReadLine());
+            int rows = matrixSize;
+            int columns = matrixSize;
+            int[][] matrix = new int[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currRowValues = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+                matrix[row] = currRowValues;
+                for (int col = 0; col < currRowValues.Length; col++)
+                {
+                    matrix[row][col] = currRowValues[col];
+                }
+            }
+
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "END")
+            {
+                string[] cmdArgs = cmd
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string action = cmdArgs[0];
+                int row = int.Parse(cmdArgs[1]);
+                int col = int.Parse(cmdArgs[2]);
+                int currValue = int.Parse(cmdArgs[3]);
+
+                if (row > rows - 1 || col > columns - 1 ||
+                    row < 0 || col < 0)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+
+                else if (action == "Add")
+                {
+                    matrix[row][col] += currValue;
+                }
+
+                else if (action == "Subtract")
+                {
+                    matrix[row][col] -= currValue;
+                }
+            }
+
+            foreach (int[] row in matrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
         }
     }
 }
