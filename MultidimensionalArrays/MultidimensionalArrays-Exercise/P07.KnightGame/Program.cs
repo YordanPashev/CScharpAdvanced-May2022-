22c1bd85060faa41cb22375e0d2b4117d2582e6f
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-04-28 22:25:42 +0300

    Adding Solution to P08

--- MultidimensionalArrays/MultidimensionalArrays-Exercise/P07.KnightGame/Program.cs
+++ MultidimensionalArrays/MultidimensionalArrays-Exercise/P07.KnightGame/Program.cs
@@ -6,7 +6,129 @@ namespace P07.KnightGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int chessBoardSize = int.Parse(Console.ReadLine());
+            int rows = chessBoardSize;
+            int columns = chessBoardSize;
+            char[,] chessBoard = CreateChessBoard(rows, columns);
+
+            bool hasAttackingKnights = true;
+            int removedKnightCounter = 0;
+
+            while (hasAttackingKnights)
+            {
+                int maxAttack = 0;
+                int currKnightAttackCount = 0;
+                int[] maxAttackKnightCoordinates = new int[2];
+
+                for (int row = 0; row < rows; row++)
+                {
+                    for (int col = 0; col < columns; col++)
+                    {
+                        if (chessBoard[row, col] == 'K')
+                        {
+                            currKnightAttackCount = GetAttack(chessBoard, row, col);
+                        }
+
+                        if (maxAttack < currKnightAttackCount)
+                        {
+                            maxAttack = currKnightAttackCount;
+                            maxAttackKnightCoordinates[0] = row;
+                            maxAttackKnightCoordinates[1] = col;
+
+                        }
+                    }
+                }
+
+                if (maxAttack == 0)
+                {
+                    hasAttackingKnights = false;
+                    break;
+                }
+
+                else
+                {
+                    int row = maxAttackKnightCoordinates[0];
+                    int col = maxAttackKnightCoordinates[1];
+                    chessBoard[row, col] = '0';
+                    removedKnightCounter++;
+                }
+            }
+
+            Console.WriteLine(removedKnightCounter);
+        }
+        static int GetAttack(char[,] chessBoard, int row, int col)
+        {
+            int attackCounter = 0;
+
+            if (IsInRange(chessBoard, row - 2, col + 1) && (chessBoard[row - 2, col + 1] == 'K'))
+            {
+                attackCounter++;
+            }
+
+            if (IsInRange(chessBoard, row - 1, col + 2) && (chessBoard[row - 1, col + 2] == 'K'))
+            {
+                attackCounter++;
+            }
+
+            if (IsInRange(chessBoard, row + 1, col + 2) && (chessBoard[row + 1, col + 2] == 'K'))
+            {
+                attackCounter++;
+            }
+
+            if (IsInRange(chessBoard, row + 2, col + 1) && (chessBoard[row + 2, col + 1] == 'K'))
+            {
+                attackCounter++;
+            }
+
+            if (IsInRange(chessBoard, row + 2, col - 1) && (chessBoard[row + 2, col - 1] == 'K'))
+            {
+                attackCounter++;
+            }
+
+            if (IsInRange(chessBoard, row + 1, col - 2) && (chessBoard[row + 1, col - 2] == 'K'))
+            {
+                attackCounter++;
+            }
+
+            if (IsInRange(chessBoard, row - 1, col - 2) && (chessBoard[row - 1, col - 2] == 'K'))
+            {
+                attackCounter++;
+            }
+
+            if (IsInRange(chessBoard, row - 2, col - 1) && (chessBoard[row - 2, col - 1] == 'K'))
+            {
+                attackCounter++;
+            }
+
+            return attackCounter;
+        }
+
+        static bool IsInRange(char[,] chessBoard, int row, int col)
+        {
+            if (row < 0 || row > chessBoard.GetLength(0) - 1 ||
+                col > chessBoard.GetLength(1) - 1 || col < 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        static char[,] CreateChessBoard(int rows, int columns)
+        {
+            char[,] chessBoard = new char[rows, columns];
+            for (int row = 0; row < rows; row++)
+            {
+                string currRowValues = Console.ReadLine();
+
+                for (int col = 0; col < columns; col++)
+                {
+                    int index = col;
+                    chessBoard[row, col] = currRowValues[index];
+                }
+            }
+
+            return chessBoard;
         }
     }
 }
---
+++ MultidimensionalArrays/MultidimensionalArrays-Exercise/P08.Bombs/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+
+namespace P08.Bombs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int matrixSize = int.Parse(Console.ReadLine());
+            int rows = matrixSize;
+            int columns = matrixSize;
+            int[,] field = new int[rows, columns];
+
+            AddAllValuesToMatrix(field, rows, columns);
+
+            string[] bombCoordinates = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            for (int i = 0; i < bombCoordinates.Length; i++)
+            {
+                int[] currBombCoordinates = bombCoordinates[i]
+                    .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => int.Parse(x))
+                    .ToArray();
+                int row = currBombCoordinates[0];
+                int col = currBombCoordinates[1];
+
+                TryToDetonateCurrBom(field, row, col);
+            }
+
+            PrintResult(field);
+        }
+
+        static int[,] AddAllValuesToMatrix(int[,] field, int rows, int columns)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currRowValues = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => int.Parse(x))
+                    .ToArray();
+
+                for (int col = 0; col < columns; col++)
+                {
+                    int index = col;
+                    field[row, col] = currRowValues[index];
+                }
+            }
+
+            return field;
+        }
+
+        static void TryToDetonateCurrBom(int[,] field, int row, int col)
+        {
+            int bombValue = field[row, col];
+
+            if (field[row, col] <= 0)
+            {
+                return;
+            }
+
+            field[row, col] = 0;
+
+            int[,] bombArea = new int[8, 2]
+            {
+               { row - 1, col - 1 },
+               { row - 1, col },
+               { row - 1, col + 1 },
+               { row, col - 1 },
+               { row, col + 1 },
+               { row + 1, col - 1 },
+               { row + 1, col },
+               { row + 1, col + 1 },
+            };
+
+            for (int i = 0; i < bombArea.GetLength(0); i++)
+            {
+                int currRow = bombArea[i, 0];
+                int currCol = bombArea[i, 1];
+
+                if (currRow > field.GetLength(0) - 1 || currRow < 0 ||
+                    currCol > field.GetLength(1) - 1 || currCol < 0)
+                {
+                    continue;
+                }
+
+                if (field[currRow, currCol] > 0)
+                {
+                    field[currRow, currCol] -= bombValue;
+                }
+            }
+        }
+
+        static void PrintResult(int[,] field)
+        {
+            int activeCellCounter = 0;
+            int activeCellSum = 0;
+
+            foreach (int digit in field)
+            {
+                if (digit > 0)
+                {
+                    activeCellSum += digit;
+                    activeCellCounter++;
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {activeCellCounter}");
+            Console.WriteLine($"Sum: {activeCellSum}");
+
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    Console.Write(field[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
