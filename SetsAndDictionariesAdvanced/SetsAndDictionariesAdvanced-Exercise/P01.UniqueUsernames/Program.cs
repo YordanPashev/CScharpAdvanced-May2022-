062546866f6566f3e2a247e48e7144315c98ecfc
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-02 00:51:43 +0300

    Adding Solution to P01 and All Problems DEscription

---
+++ SetsAndDictionariesAdvanced/SetsAndDictionariesAdvanced-Exercise/P01.UniqueUsernames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.UniqueUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> dataBase = new HashSet<string>();
+
+            int numberOfUsers = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfUsers; i++)
+            {
+                string currUsername = Console.ReadLine();
+                dataBase.Add(currUsername);
+            }
+
+            dataBase.ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
