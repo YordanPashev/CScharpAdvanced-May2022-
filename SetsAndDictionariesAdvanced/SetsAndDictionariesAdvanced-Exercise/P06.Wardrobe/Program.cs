e07e949f922ef6cb374e910b1388854a71548fd1
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-03 00:19:08 +0300

    Adding Solution to P06 and P07

---
+++ SetsAndDictionariesAdvanced/SetsAndDictionariesAdvanced-Exercise/P06.Wardrobe/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P06.Wardrobe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> dataBase = new Dictionary<string, Dictionary<string, int>>();
+
+            int numebrOfLines = int.Parse(Console.ReadLine());
+
+            FillWardrobe(dataBase, numebrOfLines);
+
+            string[] itemToCheckSpec = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            DisplayAllClothesByColor(dataBase, itemToCheckSpec);
+        }
+
+        private static void FillWardrobe(Dictionary<string, Dictionary<string, int>> dataBase, int numebrOfLines)
+        {
+            for (int i = 0; i < numebrOfLines; i++)
+            {
+                string[] currLineInfo = Console.ReadLine()
+                    .Split(" -> ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string color = currLineInfo[0];
+                string[] clothesInCurrColor = currLineInfo[1]
+                    .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                if (!dataBase.ContainsKey(color))
+                {
+                    dataBase.Add(color, new Dictionary<string, int>());
+                }
+
+                foreach (string item in clothesInCurrColor)
+                {
+                    if (!dataBase[color].ContainsKey(item))
+                    {
+                        dataBase[color].Add(item, 0);
+                    }
+
+                    dataBase[color][item]++;
+                }
+            }
+        }
+
+        private static void DisplayAllClothesByColor(Dictionary<string, Dictionary<string, int>> dataBase, string[] itemToCheckSpec)
+        {
+            string colorToChek = itemToCheckSpec[0];
+            string itemToCkeck = itemToCheckSpec[1];
+
+            foreach (var color in dataBase)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+
+                foreach (var item in color.Value)
+                {
+                    if (colorToChek == color.Key && itemToCkeck == item.Key)
+                    {
+                        Console.WriteLine($"* {item.Key} - {item.Value} (found!)");
+                        continue;
+                    }
+
+                    Console.WriteLine($"* {item.Key} - {item.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ SetsAndDictionariesAdvanced/SetsAndDictionariesAdvanced-Exercise/P07.TheV-Logger/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07.TheV_Logger
+{
+    internal class Program
+    {
+        class Vlogger
+        {
+            public Vlogger()
+            {
+                this.Followers = new SortedSet<string>();
+                this.Following = 0;
+            }
+            public SortedSet<string> Followers { get; set; } = new SortedSet<string>();
+            public int Following  { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Dictionary<string, Vlogger> dataBase = new Dictionary<string, Vlogger>();
+
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "Statistics")
+            {
+                string[] cmdArgs = cmd
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string action = cmdArgs[1];
+                string vlogger = cmdArgs[0];
+
+                if (action == "joined")
+                {
+                    TryToAddUserToDataBase(dataBase, vlogger);
+                }
+
+                else if (action == "followed")
+                {
+                    string vloggerToFollow = cmdArgs[2];
+                    TryToFollow(dataBase, vlogger, vloggerToFollow);
+                }
+            }
+
+            DisplayAllVlogersInfo(dataBase);
+        }
+
+        private static void DisplayAllVlogersInfo(Dictionary<string, Vlogger> dataBase)
+        {
+            int place = 1;
+            foreach (var vlogger in dataBase.OrderByDescending(v => v.Value.Followers.Count).ThenBy(v => v.Value.Following))
+            {
+                if (place == 1)
+                {
+                    Console.WriteLine($"{place}. {vlogger.Key} : {vlogger.Value.Followers.Count} followers, {vlogger.Value.Following} following");
+
+                    foreach (var follower in vlogger.Value.Followers)
+                    {
+                        Console.WriteLine($"*  {follower}");
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine($"{place}. {vlogger.Key} : {vlogger.Value.Followers.Count} followers, {vlogger.Value.Following} following");
+                }
+
+                place++;
+            }
+           
+
+
+
+        }
+
+        private static void TryToFollow(Dictionary<string, Vlogger> dataBase, string vlogger, string vloggerToFollow)
+        {
+            if (!dataBase.ContainsKey(vloggerToFollow) || !dataBase.ContainsKey(vlogger) ||
+                vloggerToFollow == vlogger || dataBase[vloggerToFollow].Followers.Contains(vlogger))
+            {
+                return;
+            }
+
+            else
+            {
+                dataBase[vloggerToFollow].Followers.Add(vlogger);
+                dataBase[vlogger].Following++;
+            }
+        }
+
+        private static void TryToAddUserToDataBase(Dictionary<string, Vlogger> dataBase, string vlogger)
+        {
+            if (!dataBase.ContainsKey(vlogger))
+            {
+                Vlogger vloggerInfo = new Vlogger();
+                dataBase.Add(vlogger, vloggerInfo);
+            }
+        }
+    }
+}
