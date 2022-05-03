3cb991e60aac41dd1777998270fb910f15c0d001
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-04 00:01:35 +0300

    Adding Solution to P09 and P10

---
+++ SetsAndDictionariesAdvanced/SetsAndDictionariesAdvanced-Exercise/P09.SoftUniExamResults/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P09.SoftUniExamResults
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> languageData = new Dictionary<string, int>();
+            Dictionary<string, int> usersData = new Dictionary<string, int>();
+
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "exam finished")
+            {
+                ExecuteCommand(usersData, languageData, cmd);
+            }
+
+            DisplayResults(usersData, languageData);
+        }
+
+        static void ExecuteCommand(Dictionary<string, int> usersData, Dictionary<string, int> languageData, string cmd)
+        {
+            string[] cmdArgs = cmd
+                    .Split('-', StringSplitOptions.RemoveEmptyEntries);
+
+            string username = cmdArgs[0];
+            string action = cmdArgs[1];
+
+            if (action == "banned")
+            {
+                usersData.Remove(username);
+                return;
+            }
+
+            string language = cmdArgs[1];
+            int points = int.Parse(cmdArgs[2]);
+
+            if (!languageData.ContainsKey(language))
+            {
+                languageData.Add(language, 0);
+            }
+
+            if (!usersData.ContainsKey(username))
+            {
+                usersData.Add(username, points);
+            }
+
+            else if (usersData[username] < points)
+            {
+                usersData[username] = points;
+            }
+
+            languageData[language]++;
+        }
+        private static void DisplayResults(Dictionary<string, int> usersData, Dictionary<string, int> languageData)
+        {
+            Console.WriteLine("Results:");
+            foreach (var user in usersData.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{user.Key} | {user.Value}");
+            }
+
+            Console.WriteLine($"Submissions:");
+            foreach (var language in languageData.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{language.Key} - {language.Value}");
+            }
+        }
+    }
+}
---
+++ SetsAndDictionariesAdvanced/SetsAndDictionariesAdvanced-Exercise/P10.ForceBook/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P10.ForceBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, SortedSet<string>> sides = new Dictionary<string, SortedSet<string>>();
+
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "Lumpawaroo")
+            {
+                string[] cmdArgs = cmd
+                        .Split(new string[] { " | ", " -> " }, StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+
+                if (cmd.Contains(" | "))
+                {
+                    string forceSide = cmdArgs[0];
+                    string forceUser = cmdArgs[1];
+
+                    bool IsContainsForceUser = sides.Any(x => x.Value.Contains(forceUser));
+
+                    if (!IsContainsForceUser)
+                    {
+                        if (!sides.ContainsKey(forceSide))
+                        {
+                            sides.Add(forceSide, new SortedSet<string>() { forceUser });
+                        }
+
+                        else if (!sides[forceSide].Contains(forceUser))
+                        {
+                            sides[forceSide].Add(forceUser);
+                        }
+                    }
+                }
+
+                else if (cmd.Contains(" -> "))
+                {
+                    string forceUser = cmdArgs[0];
+                    string forceSide = cmdArgs[1];
+                    bool isContainsForceUser = sides.Any(x => x.Value.Contains(forceUser));
+
+                    if (isContainsForceUser)
+                    {
+                        var currUserSide = sides.Where(x => x.Value.Contains(forceUser)).ToDictionary(x => x.Key, x => x.Value);
+
+                        if (!sides.ContainsKey(forceSide))
+                        {
+                            sides.Add(forceSide, new SortedSet<string>());
+                        }
+
+                        sides[currUserSide.First().Key].Remove(forceUser);
+                        sides[forceSide].Add(forceUser);
+                    }
+                    else
+                    {
+                        if (!sides.ContainsKey(forceSide))
+                        {
+                            sides.Add(forceSide, new SortedSet<string>());
+                        }
+
+                        sides[forceSide].Add(forceUser);
+                    }
+
+                    Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                }
+            }
+
+            DisplayForceSidesUsers(sides);
+        }
+
+        private static void DisplayForceSidesUsers(Dictionary<string, SortedSet<string>> sides)
+        {
+            foreach (var forceSide in sides.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                if (forceSide.Value.Count == 0)
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"Side: {forceSide.Key}, Members: {forceSide.Value.Count}");
+                foreach (var forceUser in forceSide.Value)
+                {
+                    Console.WriteLine($"! {forceUser}");
+                }
+            }
+        }
+    }
+}
