4bb03e6ed639169187587e83f95572651354225c
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-05 23:28:39 +0300

    Adding Solution to WordCount and MergeFiles"

--- StreamsFilesAndDirectories/Streams,FilesAndDirectories-Lab/MergeFiles/MergeFiles.cs
+++ StreamsFilesAndDirectories/Streams,FilesAndDirectories-Lab/MergeFiles/MergeFiles.cs
@@ -1,6 +1,7 @@
 ﻿namespace MergeFiles
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     public class MergeFiles
     {
@@ -15,7 +16,60 @@
 
         public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
         {
-            
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                List<string> firstTextLines = new List<string>();
+                List<string> secondTextLines = new List<string>();
+
+                using (StreamReader readerOne = new StreamReader(firstInputFilePath))
+                {
+                    string line;
+                    while ((line = readerOne.ReadLine()) != null)
+                    {
+                        firstTextLines.Add(line);
+                    }
+                }
+
+                using (StreamReader readerTwo = new StreamReader(secondInputFilePath))
+                {
+                    string line;
+                    while ((line = readerTwo.ReadLine()) != null)
+                    {
+                        secondTextLines.Add(line);
+                    }
+                }
+
+                int numberOflineForLoop = 0;
+                if (firstTextLines.Count >= secondTextLines.Count)
+                {
+                    for (int i = 0; i < firstTextLines.Count; i++)
+                    {
+                        if (i >= secondTextLines.Count)
+                        {
+                            writer.WriteLine(firstTextLines[i]);
+                            continue;
+                        }
+
+                        writer.WriteLine(firstTextLines[i]);
+                        writer.WriteLine(secondTextLines[i]);
+                    }
+                }
+
+                else
+                {
+                    for (int i = 0; i < secondTextLines.Count; i++)
+                    {
+                        if (i >= firstTextLines.Count)
+                        {
+                            writer.WriteLine(secondTextLines[i]);
+                            continue;
+                        }
+                        writer.WriteLine(firstTextLines[i]);
+                        writer.WriteLine(secondTextLines[i]);
+
+                    }
+                }     
+            }
         }
     }
 }
--- StreamsFilesAndDirectories/Streams,FilesAndDirectories-Lab/WordCount/WordCount.cs
+++ StreamsFilesAndDirectories/Streams,FilesAndDirectories-Lab/WordCount/WordCount.cs
@@ -17,6 +17,42 @@
 
         public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
         {
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                Dictionary<string, int> words = new Dictionary<string, int>();
+
+                using (StreamReader wordsToMatch = new StreamReader(wordsFilePath))
+                {
+                    string line = wordsToMatch.ReadToEnd();
+
+                    string[] arrayOfWords = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    foreach (string currWord in arrayOfWords)
+                    {
+                        words.Add(currWord.ToLower(), 0);
+                    }
+
+
+                    using (StreamReader textReader = new StreamReader(textFilePath))
+                    {
+                        string[] text = textReader.ReadToEnd().Split(new string[] { ".", "?", "!", " ", ";", ":", ",", "-", "\r", "\n", "…" }, StringSplitOptions.RemoveEmptyEntries);
+                        
+                        foreach (string currWord in text)
+                        {
+                            string  wordToCheck = currWord.ToLower();
+                            if (words.ContainsKey(wordToCheck))
+                            {
+                                words[wordToCheck]++;
+                            }
+
+                        }
+                    }
+                }
+
+                foreach (var currWord in words)
+                {
+                    writer.WriteLine($"{currWord.Key} - {currWord.Value}");
+                }
+            }
         }
     }
 }
