973af90d467d6ecb9384d6dad3e7036fcb7d1134
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-07 23:49:11 +0300

    Fixit Solution for Problem CopyBinaryFile

---
+++ StreamsFilesAndDirectories/Streams,FilesAndDirectoriesc-Exercise/CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CopyBinaryFile
+{
+    public class CopyBinaryFile
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\copyMe.png";
+            string outputPath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputPath, outputPath);
+
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using (FileStream writer = new FileStream(outputFilePath, FileMode.CreateNew))
+            {
+                using (FileStream reader = new FileStream(inputFilePath, FileMode.Open))
+                {
+                    byte[] arrayOfBytes = new byte[1024];
+
+                    int currBytes;
+                    while ((currBytes = reader.Read(arrayOfBytes, 0, arrayOfBytes.Length)) != 0)
+                    {
+                        writer.Write(arrayOfBytes, 0, arrayOfBytes.Length);
+                    }
+                }
+            }
+        }
+    }
+}
