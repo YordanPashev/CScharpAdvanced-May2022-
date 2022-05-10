adfb94a056f64980cb0e1f449cb3f902af2f9353
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-11 00:23:09 +0300

    Adding Solution to P04 and P05

--- FunctionalProgramming/FunctionalProgramming-Lab/P04.AddVAT/Program.cs
+++ FunctionalProgramming/FunctionalProgramming-Lab/P04.AddVAT/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace P04.AddVAT
 {
@@ -6,7 +9,16 @@ namespace P04.AddVAT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<decimal> prices = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .ToList();
+            Func<decimal, decimal> addVAT = x => x * 1.2m;
+
+            List<decimal> pricesWithVAT = prices.Select(addVAT).ToList();
+
+            pricesWithVAT.ForEach(x => Console.WriteLine($"{x:F2}"));
+
         }
     }
 }
--- FunctionalProgramming/FunctionalProgramming-Lab/P05.FilterByAge/Program.cs
+++ FunctionalProgramming/FunctionalProgramming-Lab/P05.FilterByAge/Program.cs
@@ -1,13 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace P05.FilterByAge
 {
+    class Student
+    {
+        public Student(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-         
+            List<Student> dataBase = new List<Student>();
+            int inputLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputLines; i++)
+            {
+                string[] currPersonInfo = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string name = currPersonInfo[0];
+                int age = int.Parse(currPersonInfo[1]);
+                dataBase.Add(new Student(name, age));
+            }
+
+            string condition = Console.ReadLine();
+            int ageRestriciton = int.Parse(Console.ReadLine());
+            string format = Console.ReadLine();
+
+            Func<Student, int, bool> filter = GetFiler(condition);
+            dataBase = dataBase.Where(s => filter(s, ageRestriciton)).ToList();
+            Action<Student> print = GetPrinter(format);
+            dataBase.ForEach(print);
+        }
+
+        private static Action<Student> GetPrinter(string format)
+        {
+            switch (format)
+            {
+                case "name":
+                    return s => Console.WriteLine(s.Name);
+                case "age":
+                    return s => Console.WriteLine(s.Age);
+                case "name age":
+                    return s => Console.WriteLine($"{s.Name} - {s.Age}");
+                default:
+                    return null;
+            }
+        }
+
+        private static Func<Student, int, bool> GetFiler(string condition)
+        {
+            switch (condition)
+            {
+                case "older":
+                    return (s, ageREstriction) => s.Age >= ageREstriction;
+                case "younger":
+                    return (s, ageREstriction) => s.Age < ageREstriction;
+                default:
+                    return null;
+            }
         }
     }
 }
