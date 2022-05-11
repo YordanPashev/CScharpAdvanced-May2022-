167a03242a435bf59bfdc227d92c92e011c10f84
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-11 19:27:36 +0300

    Adding Problems DEscription and Solution to P01, 02, 03, 04 and 05

---
+++ FunctionalProgramming/FunctionalProgramming-Exercise/P01.ActionPrint/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P01.ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Action<string> printName = n => Console.WriteLine(n);
+
+            names.ForEach(x => printName(x));
+        }
+    }
+}
---
+++ FunctionalProgramming/FunctionalProgramming-Exercise/P03.CustomMinFunction/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfDigits = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            Func<int[], int> smallestNumber = arrayOfDigits =>
+            {
+                int minValue = int.MaxValue;
+                int result = 0;
+
+                foreach (int currDigit in arrayOfDigits)
+                {
+                    if (currDigit < minValue)
+                    {
+                        result = currDigit;
+                        minValue = currDigit;
+                    }
+                }
+
+                return result;
+            };
+
+
+            Console.WriteLine(smallestNumber(arrayOfDigits));
+        }
+    }
+}
