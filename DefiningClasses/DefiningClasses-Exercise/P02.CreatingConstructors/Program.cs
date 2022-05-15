76cda1d3bb7f77c0eb4551da1fdadfef93de36c6
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-15 14:33:45 +0300

    Adding Solution to P02

---
+++ DefiningClasses/DefiningClasses-Exercise/P02.CreatingConstructors/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.WriteLine(person.Name + " " + person.Age);
+            person.Name = "George";
+            person.Age = 18;
+
+            Console.WriteLine($"{person.Name} is {person.Age} years old.");
+
+        }
+    }
+}
