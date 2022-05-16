1395e0d464f5ced20078797dc6fc0efcb74b71b0
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-17 00:03:13 +0300

    Adding solution to P10

---
+++ DefiningClasses/DefiningClasses-Exercise/P10.SoftuniParking/Parking.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private List<Car> cars;
+
+        private int capacity;
+
+        public int Count { get { return cars.Count; } }
+        public Parking(int capacity)
+        {
+            cars = new List<Car>();
+            this.capacity = capacity;
+        }
+
+        public string AddCar(Car car)
+        {
+
+            if (cars.Any(c => c.RegistrationNumber == car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            else if (this.capacity == cars.Count)
+            {
+                return "Parking is full!";
+            }
+
+            else
+            {
+                cars.Add(car);
+                return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+            }
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            if (!cars.Any(c => c.RegistrationNumber == registrationNumber))
+            {
+                return $"Car with that registration number, doesn't exist!";
+            }
+
+            else
+            {
+                Car carToRemove = cars.FirstOrDefault(c => c.RegistrationNumber == registrationNumber);
+                cars.Remove(carToRemove);
+               return $"Successfully removed {registrationNumber}";
+            }
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            return cars.Find(c => c.RegistrationNumber == registrationNumber);
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (string regNum in registrationNumbers)
+            {
+                Car carToRemove = cars.FirstOrDefault(c => c.RegistrationNumber == regNum);
+                cars.Remove(carToRemove);
+            }
+        }
+    }
+}
