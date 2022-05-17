d5a5cf15caf25c24a555b8feccd16d8bad4d7f7d
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-17 23:46:05 +0300

    Creating Class for DoublyLinkedList with methods AddFrst, AddLast and ForEach

---
+++ ImplementingLinkedList/ListNode.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList
+    {
+        private class ListNode
+        {
+            public ListNode(int value)
+            {
+                this.Value = value;
+            }
+            public int Value { get; set; }
+
+            public ListNode NextNode { get; set; }
+
+            public ListNode PreviousNode { get; set; }
+
+        }
+        private ListNode head;
+        private ListNode tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(int element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail =  new ListNode(element);
+            }
+
+            else
+            {
+                var newHead = new ListNode(element);
+                newHead.NextNode = this.head;
+                this.head.PreviousNode = newHead;
+                this.head = newHead;
+            }
+            this.Count++;
+        }
+
+        public void AddLast(int element)
+        {
+            if(Count == 0)
+            {
+                this.tail = this.head = new ListNode(element);
+            }
+
+            else if(this.Count > 0)
+            {
+                var newTail = new ListNode(element);
+                newTail.PreviousNode = this.tail;
+                this.tail.NextNode = newTail;
+                this.tail = newTail;
+            }
+            this.Count++;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.NextNode;
+            }
+        }
+    }
+}
---
+++ ImplementingLinkedList/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace CustomDoublyLinkedList
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+
+            doublyLinkedList.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
