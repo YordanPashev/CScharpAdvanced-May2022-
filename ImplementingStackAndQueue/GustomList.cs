8c097b86e9090748a33d1270149236a1cf1ffc41
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-19 22:55:15 +0300

    Creating Class GustomList with methods Add, RemoveAt and Shrink

---
+++ ImplementingStackAndQueue/GustomList.cs
@@ -0,0 +1,57 @@
+﻿namespace CustomDataStructures
+{
+    public class GustomList
+    {
+        private int[] elements;
+        private int internalCounter;
+        public GustomList()
+        {
+            elements = new int[2];
+
+        }
+
+        public void Add(int element)
+        {
+            if (elements.Length == internalCounter)
+            {
+                int[] copyArray = new int[elements.Length * 2];
+
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    copyArray[i] = elements[i];
+                }
+
+                elements = copyArray; 
+            }
+
+            elements[internalCounter] = element;
+            internalCounter++;
+        }
+
+        public void Shrink()
+        {
+            int[] newArray = new int[internalCounter];
+
+            for (int i = 0;i < internalCounter; i++)
+            {
+                newArray[i] = elements[i];
+            }
+
+            elements =  newArray;
+        }
+
+        public int RemoveAt(int index)
+        {
+            int elementToRemove = elements[index];
+
+            for (int i = index; i < internalCounter - 1; i++)
+            {
+                elements[i] = elements[i + 1];
+            }
+
+            internalCounter--;
+
+            return elementToRemove;
+        }
+    }
+}
---
+++ ImplementingStackAndQueue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomDataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GustomList gustonList = new GustomList();
+            gustonList.Add(1);
+            gustonList.Add(2);
+            gustonList.Add(3);
+            gustonList.Add(4);
+            gustonList.Add(5);
+
+            gustonList.RemoveAt(1);
+            gustonList.Shrink();
+        }
+    }
+}
