085c9f003de46384ff929bddb0d5ad08f0b05be9
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-22 17:11:48 +0300

    Adding Problems Description and solutions to P01 and P02

---
+++ Generics/Generics-Exercise/P02.GenericBoxOfInteger/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        private T digit;
+
+        public Box(T digit)
+        {
+            this.digit = digit;
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {this.digit}";
+        }
+    }
+}
---
+++ Generics/Generics-Exercise/P02.GenericBoxOfInteger/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericBoxOfInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOFDigits = int.Parse(Console.ReadLine());
+            List<Box<int>> digits = new List<Box<int>>();
+
+            for (int i = 0; i < numberOFDigits; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                Box<int> digit = new Box<int>(input);
+                digits.Add(digit);
+            }
+
+            foreach (Box<int> digit in digits)
+            {
+                Console.WriteLine(digit);
+            }
+        }
+    }
+}
