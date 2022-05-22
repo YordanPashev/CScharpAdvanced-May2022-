96c590d135db1229d97c8dcbbc6af0b09f681ae2
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-22 11:36:49 +0300

    Adding solution to P03

---
+++ Generics/Generics-Lab/P03.GenericScale/EqualityScale.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        public EqualityScale(T rightElement, T leftElement)
+        {
+            this.RightElement = rightElement;
+            this.LeftElement = leftElement;
+        }
+
+        public T RightElement { get; set; }
+        public T LeftElement { get; set; }
+
+        public bool AreEqual()
+        {
+            bool result = this.LeftElement.Equals(this.RightElement);
+            return result;
+        }
+    }
+}
---
+++ Generics/Generics-Lab/P03.GenericScale/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<string> twoElementsToCompare = new EqualityScale<string>("test", "test");
+            Console.WriteLine(twoElementsToCompare.AreEqual());
+        }
+    }
+}
