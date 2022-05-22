b9587a149c527659d3de304d73841473323948ca
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-22 17:14:17 +0300

    Adding solution to P01

---
+++ Generics/Generics-Exercise/P01.GenericBoxofString/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxofString
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType()}: {this.Value}";
+        }
+    }
+}
---
+++ Generics/Generics-Exercise/P01.GenericBoxofString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericBoxofString
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Box<string>> listOfStrings = new List<Box<string>>();
+
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string input = Console.ReadLine();
+                Box<string> box = new Box<string>(input);
+                listOfStrings.Add(box);
+            }
+
+            foreach (Box<string> box in listOfStrings)
+            {
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
