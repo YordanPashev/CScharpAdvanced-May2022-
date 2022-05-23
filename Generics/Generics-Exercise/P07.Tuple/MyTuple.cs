1f7150634b460f44f57112b68f1df3b9badedf64
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-23 23:14:42 +0300

    Addin solution to P03

---
+++ Generics/Generics-Exercise/P07.Tuple/MyTuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class MyTuple<T1, T2>
+    {
+        public MyTuple(T1 item1, T2 item2)
+        {
+            this.ItemOne = item1;
+            this.ItemTwo = item2;
+        }
+
+        public T1 ItemOne { get; set; }
+
+        public T2 ItemTwo { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{this.ItemOne} -> {this.ItemTwo}";
+        }
+    }
+}
---
+++ Generics/Generics-Exercise/P07.Tuple/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] firstInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            string names = firstInput[0] + " " + firstInput[1];
+            string address = firstInput[2];
+
+            MyTuple<string, string> firstTupel = new MyTuple<string, string>(names, address);
+
+            string[] secondInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            string name = secondInput[0];
+            int amountOfBeerInLiters = int.Parse(secondInput[1]);
+            MyTuple<string, int> secondTuplel = new MyTuple<string, int>(name, amountOfBeerInLiters);
+
+            string[] thirdInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            int integerValue = int.Parse(thirdInput[0]);
+            double doubleValue = double.Parse(thirdInput[1]);
+            MyTuple<int, double> thidthTuplel = new MyTuple<int, double>(integerValue, doubleValue);
+
+            Console.WriteLine(firstTupel);
+            Console.WriteLine(secondTuplel);
+            Console.WriteLine(thidthTuplel);
+        }
+    }
+}
