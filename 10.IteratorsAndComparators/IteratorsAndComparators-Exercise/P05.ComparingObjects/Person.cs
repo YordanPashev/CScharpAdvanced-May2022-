589ab1f8b421aadfabc8f04358bd5ed8de63721f
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-06-07 00:56:20 +0300

    Adding Solution to P05

---
+++ 10.IteratorsAndComparators/IteratorsAndComparators-Exercise/P05.ComparingObjects/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Town { get; set; }
+
+        public List<Person> MyProperty { get; set; }
+
+        public int CompareTo(Person person)
+        {
+            int comareResultByNames = this.Name.CompareTo(person.Name);
+            if (comareResultByNames != 0)
+            {
+                return comareResultByNames;
+            }
+
+            int comareResultByAge = this.Age.CompareTo(person.Age);
+            if (comareResultByAge != 0)
+            {
+                return comareResultByAge;
+            }
+
+            int comareResultByTown = this.Town.CompareTo(person.Town);
+            if (comareResultByTown != 0)
+            {
+                return comareResultByTown;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ 10.IteratorsAndComparators/IteratorsAndComparators-Exercise/P05.ComparingObjects/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComparingObjects
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "END")
+            {
+                string[] cmdArgs = cmd
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string name = cmdArgs[0];
+                int age = int.Parse(cmdArgs[1]);
+                string town = cmdArgs[2];
+                Person newPersonToAdd = new Person(name, age, town);
+                persons.Add(newPersonToAdd);
+            }
+
+            int indexOfPersonToCompare = int.Parse(Console.ReadLine()) - 1;
+
+            Person personsToCompare = persons[indexOfPersonToCompare];
+            int matchCount = 0;
+            int notEaquelCount = 0;
+
+            foreach (Person person in persons)
+            {
+                if (person.CompareTo(personsToCompare) == 0)
+                {
+                    matchCount++;
+                }
+
+                else
+                {
+                    notEaquelCount++;
+                }
+            }
+
+            if (matchCount == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+
+            else
+            {
+                Console.WriteLine($"{matchCount} {notEaquelCount} {persons.Count}");
+            }
+        }
+    }
+}
