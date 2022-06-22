f4b32b1eb9b6a909347b4ae0d54d999e17fda536
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-06-23 01:50:28 +0300

    Adding Solution to P03.StockMarket

---
+++ 11.ExamPreparation/Exam - 23 October 2021/P03.StockMarket/Investor.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+        public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
+            FullName = fullName;
+            EmailAddress = emailAddress;
+            MoneyToInvest = moneyToInvest;
+            BrokerName = brokerName;
+            this.Portfolio = new List<Stock>();
+        }
+
+        public List<Stock> Portfolio { get; set; }
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal MoneyToInvest { get; set; }
+        public string BrokerName { get; set; }
+        public int Count => this.Portfolio.Count;
+
+        public void BuyStock(Stock stock)
+        {
+            if (stock.MarketCapitalization > 10000 && this.MoneyToInvest >= stock.PricePerShare)
+            {
+                this.MoneyToInvest -= stock.PricePerShare;
+                this.Portfolio.Add(stock);
+            }
+        }
+
+        public string SellStock(string companyName, decimal sellPrice)
+        {
+            Stock stock = this.Portfolio.FirstOrDefault(s => s.CompanyName == companyName);
+
+            if (stock == null)
+            {
+                return $"{companyName} does not exist.";
+            }
+
+
+            else if (sellPrice < stock.PricePerShare)
+            {
+                return $"Cannot sell {companyName}.";
+            }
+
+            this.Portfolio.Remove(stock);
+            this.MoneyToInvest += sellPrice;
+            return $"{companyName} was sold.";
+        }
+
+        public Stock FindStock(string companyName) => this.Portfolio.FirstOrDefault(s => s.CompanyName == companyName);
+
+        public Stock FindBiggestCompany() => this.Portfolio.OrderByDescending(s => s.MarketCapitalization).FirstOrDefault();
+
+        public string InvestorInformation()
+        {
+            StringBuilder InvestitorInfo = new StringBuilder();
+
+            InvestitorInfo.AppendLine($"The investor {this.FullName} with a broker {this.BrokerName} has stocks:");
+            foreach (Stock stock in this.Portfolio)
+            {
+                InvestitorInfo.AppendLine(stock.ToString());
+            }
+
+            return InvestitorInfo.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 11.ExamPreparation/Exam - 23 October 2021/P03.StockMarket/Stock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StockMarket
+{
+    public class Stock
+    {
+        public Stock(string companyName, string director, decimal pricePerShare, int totalNumberOfShares)
+        {
+            this.CompanyName = companyName;
+            this.Director = director;
+            this.PricePerShare = pricePerShare;
+            this.TotalNumberOfShares = totalNumberOfShares;
+            this.MarketCapitalization = pricePerShare * totalNumberOfShares;
+        }
+
+        public string CompanyName { get; set; }
+
+        public string Director { get; set; }
+
+        public decimal PricePerShare { get; set; }
+
+        public int TotalNumberOfShares { get; set; }
+
+        public decimal MarketCapitalization { get; set; }
+
+        public override string ToString()
+        {
+            return $"Company: {CompanyName}{Environment.NewLine}" +
+                   $"Director: { Director}{Environment.NewLine}" +
+                   $"Price per share: ${ PricePerShare}{Environment.NewLine}" +
+                   $"Market capitalization: ${ MarketCapitalization}";
+        }
+    }
+}
