1b3d250e6e5a441d6cfcd4b6a310c4b30010a928
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-02 13:25:12 +0300

    Fixing names In Subject Folders

---
+++ 12.BasicAlgorithms/P07.BinarySearch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace P07.BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            int element = int.Parse(Console.ReadLine());
+            Console.WriteLine(BinarySearch.IndexOf(arr, element));
+        }
+
+        private class BinarySearch
+        {
+            public static int IndexOf(int[] arr, int element)
+            {
+                int low = 0;
+                int high = arr.Length - 1;
+
+                while (low <= high)
+                {
+                    int middle = (low + high) / 2;
+
+                    if (element > arr[middle])
+                    {
+                        low = middle + 1;
+                    }
+
+                    else if (element < arr[middle])
+                    {
+                        high = middle - 1;
+                    }
+
+
+                    else if (arr[middle] == element)
+                    {
+                        return middle;
+                    }
+                }
+
+                return -1;
+            }
+        }
+    }
+}
